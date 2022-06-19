11866f61ce74296b3ad3554d7a2e02c51216370d
Moïse GUANNEL <mguannel97@gmail.com>
2022-06-18 21:22:12 -0400

    update models

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,6 @@
 class Car < ApplicationRecord
+  belongs_to :user
+  has_many :bookings, dependent: :destroy
+  validates :brand, :model, :address, presence: true
+  validates :price_per_day, presence: true
 end
